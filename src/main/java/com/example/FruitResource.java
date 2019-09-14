15ca5c21abff30216930e2e14924579e8e74b541
piggyvenus <piggyvenus@gmail.com>
2019-09-13 17:52:54 -0700

    test

---
+++ src/main/java/com/example/FruitResource.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/fruit")
+public class FruitResource {
+
+    @ConfigProperty(name = "greetings.message")
+    String message;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return message;
+    }
+}
